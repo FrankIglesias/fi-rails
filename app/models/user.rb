a33f5d80c40293e8b8cc15e2269bf9996586cf56
Francisco Iglesias <franciscoj.iglesias@hotmail.com>
2018-08-16 19:58:34 -0300

    Added migration to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  attr_accessor :first_name, :last_name
-  validates :first_name, presence: true
-  validates :last_name, presence: true
+
+  validates :first_name, :last_name, presence: true
 end
